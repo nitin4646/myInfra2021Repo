3a8520d01e2b331a9abc388a7db0525a368a1969
nitin4646 <52617166+nitin4646@users.noreply.github.com>
2024-01-31 20:30:36 +0530

    backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "my-s3-bucket-for-tf-state-31012024"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamo-db-table-tf31012024-1"
   }
 }
